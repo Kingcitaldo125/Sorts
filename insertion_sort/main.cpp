26dbc84a53c5d83f364339840759eadea16d857d
Paul <paularelt@gmail.com>
2021-12-01 16:33:34 -0500

    Create main.cpp

---
+++ insertion_sort/main.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using std::cout;
+
+void insertion_sort(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+    // need at least 2 elements in vector to perform swaps
+    if(std::distance(begin,end) == 1)
+        return;
+
+    for(auto i = begin + 1; i != end; ++i)
+    {
+        auto first = i-1;
+        auto i_cpy = i;
+
+        while(*i_cpy < *first)
+        {
+            if(first < begin)
+                break;
+
+            std::iter_swap(i_cpy--, first--);
+        }
+    }
+}
+
+int main()
+{
+    std::vector<int> mints {96, 65, 131, 67, 70, 72, 8, 13, 46, 144, 145, 114, 18, 20, 55, 119, 58, 91, 124};
+
+    insertion_sort(mints.begin(), mints.end());
+
+    for(auto& i : mints)
+    {
+        cout << i << " ";
+    }
+    cout << '\n';
+
+    return 0;
+}
