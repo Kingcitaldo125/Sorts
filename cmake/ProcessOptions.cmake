fc0b9dae24d273263f6cc4beae49515f4c5ecf32
Paul Arelt <paularelt@gmail.com>
2021-12-08 16:12:51 -0500

    Consolidate macro to single file; add debug logging

--- cmake/ProcessOptions.cmake
+++ cmake/ProcessOptions.cmake
@@ -1,3 +1,14 @@
+macro(process_sort_names)
+  file(GLOB file_listing LIST_DIRECTORIES true RELATIVE ${PROJECT_SOURCE_DIR} *sort/)
+
+  set(sort_names "")
+
+  foreach(x ${file_listing})
+    list(APPEND sort_names ${x})
+    message(DEBUG "Processing directory ${x}")
+  endforeach()
+endmacro()
+
 macro(process_options)
   include(${PROJECT_SOURCE_DIR}/cmake/ProcessSortNames.cmake)
 
