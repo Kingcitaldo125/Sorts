5158007e3cd37abd5771774144b3d9cb7d0d3149
Paul <paularelt@gmail.com>
2021-12-19 13:49:03 -0500

    gnome sort

---
+++ gnome_sort/main.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using std::cout;
+
+void gnome_sort(std::vector<int>::iterator begin,
+                std::vector<int>::iterator end) {
+  auto i = begin + 1;
+  while (1) {
+    if (std::distance(i, end) < 1)
+      break;
+
+    if (*(i - 1) <= *i) {
+      ++i;
+      continue;
+    } else {
+      std::iter_swap(i - 1, i);
+      --i;
+      if (std::distance(begin, i) == 0)
+        i = begin + 1;
+    }
+  }
+}
+
+int main() {
+  std::vector<int> mints{65, 28, 60, 73, 44, 84, 91, 5,  63, 8,  82, 46, 9,
+                         97, 42, 7,  51, 18, 68, 84, 70, 64, 53, 52, 50};
+
+  gnome_sort(mints.begin(), mints.end());
+
+  for (auto &i : mints) {
+    cout << i << " ";
+  }
+  cout << "\n";
+  
+  cout << "is_sorted: "
+       << (std::is_sorted(mints.begin(), mints.end()) ? "Sorted" : "UnSorted")
+       << std::endl;
+
+  return 0;
+}
