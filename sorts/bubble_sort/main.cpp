0426f5171203558dcc470929dff856c8f02d9b16
Paul <paularelt@gmail.com>
2021-12-24 11:53:38 -0500

    Optimize bubble sort

--- sorts/bubble_sort/main.cpp
+++ sorts/bubble_sort/main.cpp
@@ -5,38 +5,40 @@
 using std::cout;
 using std::endl;
 
-void print_vector(const std::vector<int> &mints) {
-  for (auto &v : mints) {
-    cout << v << " ";
-  }
-  cout << '\n';
+void print_vector(const std::vector<int>& mints) {
+    for (auto& v : mints) {
+        cout << v << " ";
+    }
+    cout << '\n';
 }
 
 void bubble_sort(std::vector<int>::iterator begin,
-                 std::vector<int>::iterator end) {
-  bool swap = true;
-  while (swap) {
-    swap = false;
-    for (auto i = begin; i != end - 1; ++i) {
-      auto second = i + 1;
-      if (*i > *second) {
-        std::iter_swap(i, second);
-        swap = true;
-      }
+    std::vector<int>::iterator end) {
+    bool swap = true;
+    unsigned long int passes = 0;
+    while (swap) {
+        swap = false;
+        for (auto i = begin; i != end - (passes + 1); ++i) {
+            auto second = i + 1;
+            if (*i > *second) {
+                std::iter_swap(i, second);
+                swap = true;
+            }
+        }
+        ++passes;
     }
-  }
 }
 
 int main() {
-  std::vector<int> mints{72, 5,  65, 97, 48, 2,  50, 41, 62,
-                         63, 44, 47, 28, 13, 27, 35, 42};
+    std::vector<int> mints{ 72, 5,  65, 97, 48, 2,  50, 41, 62,
+                           63, 44, 47, 28, 13, 27, 35, 42 };
 
-  bubble_sort(mints.begin(), mints.end());
+    bubble_sort(mints.begin(), mints.end());
 
-  print_vector(mints);
+    print_vector(mints);
 
-  cout << "is_sorted: " << std::boolalpha
-       << std::is_sorted(mints.begin(), mints.end()) << endl;
+    cout << "is_sorted: " << std::boolalpha
+        << std::is_sorted(mints.begin(), mints.end()) << endl;
 
-  return 0;
+    return 0;
 }
