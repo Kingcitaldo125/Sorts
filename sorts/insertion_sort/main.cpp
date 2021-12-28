2f2b019e34c30a2c3bc5f56deac8dbc094d0eb2f
Paul <paularelt@gmail.com>
2021-12-28 12:29:07 -0500

    Try another insertion sort fix
    to resolve MSVC compiler issue

--- sorts/insertion_sort/main.cpp
+++ sorts/insertion_sort/main.cpp
@@ -13,25 +13,25 @@ void print_vector(const std::vector<int> &mints) {
 }
 
 void insertion_sort(std::vector<int>::iterator begin,
-                    std::vector<int>::iterator end) {
-  // need at least 2 elements in vector to perform swaps
-  if (std::distance(begin, end) <= 1)
-    return;
+	std::vector<int>::iterator end) {
+	// need at least 2 elements in vector to perform swaps
+	if (std::distance(begin, end) <= 1)
+		return;
 
-  for (auto i = begin + 1; i != end; ++i) {
-    auto first = i - 1;
-    auto i_cpy = i;
+	for (auto i = begin + 1; i != end; ++i) {
+		auto first = i - 1;
+		auto i_cpy = i;
 
-    while (*i_cpy < *first) {
-      if (first < begin)
-        break;
+		while (*i_cpy < *first) {
+			std::iter_swap(i_cpy, first);
 
-      std::iter_swap(i_cpy, first);
+			if (first <= begin)
+				break;
 
-      --i_cpy;
-      --first;
-    }
-  }
+			--i_cpy;
+			--first;
+		}
+	}
 }
 
 int main() {
