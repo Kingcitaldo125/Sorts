0534e89b1dba483d692b3f27dac2556f6ad84cc3
Paul <paularelt@gmail.com>
2023-12-22 08:47:22 -0500

    Add Sleep Sort code.

---
+++ sorts/sleep_sort/main.cpp
@@ -0,0 +1,48 @@
+#include <chrono>
+#include <iostream>
+#include <list>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+static std::mutex mtx;
+
+void corot(std::vector<int> &mints, const int N) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(N));
+  {
+    std::lock_guard<std::mutex> mlock(mtx);
+    mints.push_back(N);
+  }
+}
+
+std::vector<int> sleep_sort(const std::vector<int> &mvec) {
+  std::vector<int> mints;
+  std::list<std::thread> threads;
+
+  for (const auto &i : mvec) {
+    threads.push_back(std::thread(corot, std::ref(mints), i));
+  }
+
+  for (auto &th : threads) {
+    th.join();
+  }
+
+  return mints;
+}
+
+int main() {
+  std::vector<int> mints{32,  867, 661, 347, 91,  238, 739, 820, 20,  838,
+                         796, 550, 803, 240, 327, 81,  896, 810, 151, 281,
+                         886, 106, 660, 45,  787, 674, 20,  120, 437, 25,
+                         930, 751, 3,   145, 674, 362, 809, 517, 418, 469,
+                         689, 738, 623, 842, 643, 211, 308, 13,  792, 790};
+
+  const auto res = sleep_sort(std::move(mints));
+
+  for (const auto &i : res) {
+    std::cout << i << " ";
+  }
+  std::cout << "\n";
+
+  return 0;
+}
